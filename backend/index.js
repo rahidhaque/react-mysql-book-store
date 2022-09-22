ab82bf6481f07b5fc8ec03d6b3e7a64fc6f8ef37
rahidhaque <ontirahid@yahoo.com>
2022-09-22 11:27:15 +0600

    Middleware Added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-
+const cors = require('cors');
 const app = express();
 
 const db = mysql.createConnection({
@@ -11,6 +11,7 @@ const db = mysql.createConnection({
 })
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json("hello this is the backend");
